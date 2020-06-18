6ec7b1ddc45e3a413d597a88e253bf3ce11ba7d4
TrueComputer <c-summerhill@hotmail.com>
2020-06-18 17:28:59 +0100

    Cleaning up misplaced files
    Oops

--- ShooterTutorial.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "ShooterTutorial.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ShooterTutorial, "ShooterTutorial" );
